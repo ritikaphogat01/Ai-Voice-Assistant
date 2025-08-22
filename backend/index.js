719080119d0a3d1cc5a317ce3e30b8d905d0bc1f
ritikaphogat01 <145350128+ritikaphogat01@users.noreply.github.com>
2025-08-22 12:36:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 
 app.use(cors({
-    origin:"https://ai-voice-assistant-r521.onrender.com",
+    origin:"https://ai-voice-assistant-1-mzuc.onrender.com",
     credentials:true
 }))
 
